75da626225b4fe2d3ce066335753dd6ea2b88def
PixelCyberMe <azamat1999bab@gmail.com>
2026-04-19 20:52:25 +0500

    Update Clicker.lua

--- Clicker.lua
+++ Clicker.lua
@@ -21,7 +21,6 @@ button.Parent = gui
 
 local function toggle()
     enabled = not enabled
-
     if enabled then
         button.Text = "AUTO CLICK: ON"
         print("AutoClick ON")
@@ -33,7 +32,6 @@ end
 
 button.MouseButton1Click:Connect(toggle)
 
--- ⌨️ Q toggle
 UserInputService.InputBegan:Connect(function(input, gp)
     if gp then return end
     if input.KeyCode == Enum.KeyCode.Q then
@@ -44,18 +42,15 @@ end)
 task.spawn(function()
     while true do
         task.wait(0.2)
-
         if enabled then
             local char = player.Character
             local hrp = char and char:FindFirstChild("HumanoidRootPart")
-
             if hrp then
                 for _, obj in pairs(Workspace:GetDescendants()) do
                     if obj:IsA("ClickDetector") then
                         local part = obj.Parent
                         if part and part:IsA("BasePart") then
                             local dist = (hrp.Position - part.Position).Magnitude
-
                             if dist <= 15 then
                                 fireclickdetector(obj)
                             end
@@ -64,6 +59,44 @@ task.spawn(function()
                 end
             end
         end
+    end
+end)
+
+local dragging = false
+local dragStart
+local startPos
+local dragInput
+
+button.InputBegan:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 
+    or input.UserInputType == Enum.UserInputType.Touch then
+        dragging = true
+        dragStart = input.Position
+        startPos = button.Position
+        input.Changed:Connect(function()
+            if input.UserInputState == Enum.UserInputState.End then
+                dragging = false
+            end
+        end)
+    end
+end)
+
+button.InputChanged:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseMovement 
+    or input.UserInputType == Enum.UserInputType.Touch then
+        dragInput = input
+    end
+end)
+
+UserInputService.InputChanged:Connect(function(input)
+    if dragging and input == dragInput then
+        local delta = input.Position - dragStart
+        button.Position = UDim2.new(
+            startPos.X.Scale,
+            startPos.X.Offset + delta.X,
+            startPos.Y.Scale,
+            startPos.Y.Offset + delta.Y
+        )
     end
 end)
 
