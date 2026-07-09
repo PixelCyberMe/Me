fcfd7bb6e6fcc3b5bb43f8bf98c379e52d2a1e22
PixelCyberMe <azamat1999bab@gmail.com>
2026-07-09 17:29:24 +0500

    Update sanity.lua

--- sanity.lua
+++ sanity.lua
@@ -1,14 +1,24 @@
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Library = require(ReplicatedStorage:WaitForChild("Lib"))
+local ReplicatedStorage = game:GetService("InterimStorage") or game:GetService("ReplicatedStorage")
 local LocalPlayer = Players.LocalPlayer
 
+local LibFolder = ReplicatedStorage:FindFirstChild("Lib")
+if not LibFolder then
+    print("[Error] Этот скрипт работает только в определенной хоррор-игре!")
+    return
+
+local Library = require(LibFolder)
+
 local function keepSanityFull()
-	LocalPlayer:SetAttribute("Sanity", 100)
+    if LocalPlayer then
+        LocalPlayer:SetAttribute("Sanity", 100)
+    end
 end
+
 Library.Inject("PlayerLostSanity", keepSanityFull)
 LocalPlayer:GetAttributeChangedSignal("Sanity"):Connect(keepSanityFull)
 RunService.Heartbeat:Connect(keepSanityFull)
+
 keepSanityFull()
-return {}
+print("[Success] Читы на рассудок успешно активированы!")
